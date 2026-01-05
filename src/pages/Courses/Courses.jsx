71259de1d490a4112d33d06702082cee8507acf1
NarahariVasantham1567 <naraharivasantham1567@gmail.com>
2026-01-05 18:00:35 +0530

    worked on pie chart, line chart

---
+++ src/pages/Courses/Courses.jsx
@@ -0,0 +1,11 @@
+import './Courses.css';
+
+const Courses = () => {
+  return (
+    <div className='dashboard-container'>
+      <h2 className='dashboard-heading'>Courses Overview</h2>
+    </div>
+  );
+};
+
+export default Courses;
---
+++ src/pages/Tutors/Tutors.jsx
@@ -0,0 +1,11 @@
+import './Tutors.css';
+
+const Tutors = () => {
+  return (
+    <div className='dashboard-container'>
+      <h2 className='dashboard-heading'>Tutors Overview</h2>
+    </div>
+  );
+};
+
+export default Tutors;
